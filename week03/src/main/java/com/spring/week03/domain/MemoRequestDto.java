3ff05568408d9a7995f46762b10547c5cd1fb4cc
Jun Kim <rlafbf222@naver.com>
2022-05-20 21:26:58 +0900

    3주차 메모 domain 패키지 생성까지 _ 12일차

---
+++ week03/src/main/java/com/spring/week03/domain/MemoRequestDto.java
@@ -0,0 +1,12 @@
+package com.spring.week03.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+
+    private String contents;
+
+}
