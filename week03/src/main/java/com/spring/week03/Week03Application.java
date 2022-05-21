b3f373b672c67bc708c367fb1cd16ed8c909a99b
Jun Kim <rlafbf222@naver.com>
2022-05-21 23:02:01 +0900

    익명의 메모 만들기 24시간 이내 조회 포함 완료_13일차

--- week03/src/main/java/com/spring/week03/Week03Application.java
+++ week03/src/main/java/com/spring/week03/Week03Application.java
@@ -1,8 +1,11 @@
 package com.spring.week03;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA auditing 넣어줘야함!
 @SpringBootApplication
 public class Week03Application {
 
--- week03/src/main/java/com/spring/week03/domain/MemoRepository.java
+++ week03/src/main/java/com/spring/week03/domain/MemoRepository.java
@@ -2,11 +2,12 @@ package com.spring.week03.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     // JPA의 메서드는 이름에 따라 기능을 바꿀 수 있다. 북마크에 저장된 문서 참조!
     // Memo class가 Timestamped를 상속받고 있기 때문에 ModifiedAt을 사용할 수 있는 것이다.
     // 아래 메서드는 모든 것을 찾되, 수정된 날짜를 역순으로 정렬해서 찾아라는 뜻!
-    List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime startDate, LocalDateTime endDate);
 }
